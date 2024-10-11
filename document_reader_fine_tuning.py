db51f1f0f01a413b7ad0cece23b677c0534a7679
Laura Fernandes Camargos <102076463+laurafcamargos@users.noreply.github.com>
2024-10-11 18:21:24 -0300

    up

--- document_reader_fine_tuning.py
+++ document_reader_fine_tuning.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-"""4_Document_Reader_Fine_tuning.ipynb"""
+"""Document_Reader_Fine_tuning.ipynb"""
 
 import os
+import enum
 import ast
+from utils import (
+    load_and_clean_data,
+    preprocess_function,
+    string_to_dict,
+    filter_dataset,
+)
 from datasets import load_dataset, Dataset, DatasetDict
 from transformers import (
     AutoTokenizer,
@@ -11,121 +18,70 @@ from transformers import (
     TrainingArguments,
     Trainer,
     EarlyStoppingCallback,
-    pipeline
+    pipeline,
 )
 from huggingface_hub import HfApi, login
 
-# Certifique-se de definir seu token de acesso aqui ou como variável de ambiente
+
+class DocReader:
+    """Document Reader options"""
+
+    BERT = "deepset/bert-base-uncased-squad2"
+    RoBERTa = "deepset/roberta-base-squad2"
+    MiniLM = "deepset/minilm-uncased-squad2"
+    DistilBERT = "distilbert-base-uncased-distilled-squad"
+    ELECTRA = "deepset/electra-base-squad2"
+    SmallDistilBERT = "laurafcamargos/distilbert-qasports-basket-small"
+
+
+class Sports:
+    BASKETBALL = "basketball"
+    FOOTBALL = "football"
+    SOCCER = "soccer"
+    ALL = ""
+
+
+DOC_READER = DocReader.DistilBERT
+SPORT = Sports.BASKETBALL
 HUGGINGFACE_TOKEN = "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"
 
-# Login automático utilizando o token
+
 os.environ["HUGGINGFACE_TOKEN"] = HUGGINGFACE_TOKEN
 login(token=HUGGINGFACE_TOKEN)
 
-# Função para converter string para dicionário
-def string_to_dict(string):
-    try:
-        return ast.literal_eval(string)
-    except (SyntaxError, ValueError):
-        print(f"Error evaluating string: {string}")
-        return {}
-
-# Função para limpar o dataset
-def clean_dataset(data: Dataset) -> Dataset:
-    cleaned_data = []
-    for tt in data:
-        answer = string_to_dict(tt["answer"])
-        if answer["text"] and answer["offset"][1] != 0:
-            cleaned_data.append(tt)
-    return Dataset.from_list(cleaned_data)
-
-# Carregar dataset
-train = load_dataset("PedroCJardim/QASports", "basketball", split="train")
-test = load_dataset("PedroCJardim/QASports", "basketball", split="validation")
-
-cleaned_train = clean_dataset(train)
-cleaned_test = clean_dataset(test)
-
-dataset = DatasetDict({"train": cleaned_train, "test": cleaned_test})
 
 # Tokenizador
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained(DOC_READER)
 
-# Função de pré-processamento
-def preprocess_function(examples):
-    examples["answer"] = [string_to_dict(t) for t in examples["answer"]]
-    questions = [q.strip() for q in examples["question"]]
-    inputs = tokenizer(
-        questions,
-        examples["context"],
-        max_length=512,
-        truncation="only_second",
-        return_offsets_mapping=True,
-        padding="max_length",
-    )
+# carrega o dataset
+dataset = load_and_clean_data(SPORT)
 
-    offset_mapping = inputs.pop("offset_mapping")
-    answers = examples["answer"]
-    start_positions = []
-    end_positions = []
 
-    for i, offset in enumerate(offset_mapping):
-        if not answers[i]:
-            start_positions.append(0)
-            end_positions.append(0)
-            continue
-        answer = answers[i]
-        start_char = answer["offset"][0]
-        end_char = answer["offset"][1]
-        sequence_ids = inputs.sequence_ids(i)
-
-        idx = 0
-        while sequence_ids[idx] != 1:
-            idx += 1
-        context_start = idx
-        while sequence_ids[idx] == 1:
-            idx += 1
-        context_end = idx - 1
-
-        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
-            start_positions.append(0)
-            end_positions.append(0)
-        else:
-            idx = context_start
-            while idx <= context_end and offset[idx][0] <= start_char:
-                idx += 1
-            start_positions.append(idx - 1)
-
-            idx = context_end
-            while idx >= context_start and offset[idx][1] >= end_char:
-                idx -= 1
-            end_positions.append(idx + 1)
-
-    inputs["start_positions"] = start_positions
-    inputs["end_positions"] = end_positions
-    return inputs
-
-# Aplicar pré-processamento
-tokenized_squad = dataset.map(preprocess_function, batched=True, remove_columns=dataset["train"].column_names)
+# Chama o pré-processamento apenas na coluna de test
+tokenized_squad = dataset.map(
+    lambda examples: preprocess_function(examples, tokenizer),
+    batched=True,
+    remove_columns=dataset["train"].column_names,
+)
 
 # Data collator
 data_collator = DefaultDataCollator()
 
 # Carregar modelo
-model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+model = AutoModelForQuestionAnswering.from_pretrained(DOC_READER)
 
 # Argumentos de treinamento
 training_args = TrainingArguments(
     output_dir="distilbert-qasports-basketball",
     evaluation_strategy="epoch",
-    save_strategy="epoch",  # Certifique-se de que as estratégias de salvamento e avaliação correspondem
+    save_strategy="epoch",
     learning_rate=2e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=3,  # Ajustado para 3 épocas para teste rápido
+    num_train_epochs=1000,
     weight_decay=0.01,
     push_to_hub=True,
-    load_best_model_at_end=True
+    load_best_model_at_end=True,
 )
 
 # Configurar Trainer
@@ -136,14 +92,14 @@ trainer = Trainer(
     eval_dataset=tokenized_squad["test"],
     tokenizer=tokenizer,
     data_collator=data_collator,
-    callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=5)],
 )
 
-# Treinar modelo
+# Treina o modelo
 trainer.train()
 
 # Publicar modelo no HuggingFace (descomente para usar)
-trainer.push_to_hub()
+# trainer.push_to_hub()
 
 # Inference
 question = "What team waived Jones on October 25?"
@@ -154,7 +110,9 @@ context = (
     "Philadelphia 76ers, recording one assist and two steals in seven minutes off the bench."
 )
 
-question_answerer = pipeline(task="question-answering", model=model, tokenizer=tokenizer)
+question_answerer = pipeline(
+    task="question-answering", model=model, tokenizer=tokenizer
+)
 answer = question_answerer(question=question, context=context)
+
 print(answer)
- 
